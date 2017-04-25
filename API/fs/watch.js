341776b8bfeffc879394752bfa11659dd8d90c82
pangbo <bo.pang@oborcloud.com>
2017-04-25 15:42:03 +0800

    delete API doc

--- API/fs/watch.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-fs.watch('sample.txt', function (eventType, filename) {
-    console.log('event: '+ eventType);
-    console.log('filename: '+ filename)
-});
