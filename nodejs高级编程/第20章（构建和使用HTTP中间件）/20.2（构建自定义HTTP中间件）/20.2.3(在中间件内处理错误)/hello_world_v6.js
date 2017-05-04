1d6304f0aea3a3e1ee448813f89cbab9a085230e
pangbooo <1838529223@qq.com>
2017-05-04 18:17:16 +0800

    express

---
+++ nodejs高级编程/第20章（构建和使用HTTP中间件）/20.2（构建自定义HTTP中间件）/20.2.3(在中间件内处理错误)/hello_world_v6.js
@@ -0,0 +1,17 @@
+var connect = require('connect');
+
+var dir_name = 'C:\Users\ECC01\Desktop\requests';
+
+//导入中间件
+var errorCreator = require('./error');
+var saveRquest = require('../20.2.2(在中间件内部注册回调函数)/saveRquest');
+var writeHeader = require('../20.2.1(创建异步中间件)/write_header');
+var replyText =require('../reply_text');
+
+var app = connect();
+app.use(errorCreator());
+app.use(saveRquest(dir_name));
+app.use(writeHeader('X-Powered-By', 'node'));
+app.use(replyText('Hello world v4'));
+
+app.listen(8080);
